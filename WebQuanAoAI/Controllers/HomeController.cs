658ae49f03f3cfa09d6ea2c8df80910efffd3eb8
Khang Hoàng <mkhoangvip@gmail.com>
2024-04-20 03:42:51 +0700

    tuan lam toi bai 10

--- WebQuanAoAI/Controllers/HomeController.cs
+++ WebQuanAoAI/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.AccessControl;
 using WebQuanAoAI.Models;
+using WebQuanAoAI.Repository;
 
 namespace WebQuanAoAI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _datacontext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger , ApplicationDbContext dbContext)
         {
             _logger = logger;
+            _datacontext = dbContext;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = _datacontext.Products.ToList();
+            return View(products);
         }
 
         public IActionResult Privacy()
